426b17627fcbf79af2824805283ceff32100a853
Naomi <naeimehnour@gmail.com>
2024-07-03 02:57:45 +0100

    Utility functions completed

--- src/textsummarizer/utils/common.py
+++ src/textsummarizer/utils/common.py
@@ -34,3 +34,34 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
+    
+
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """
+    Creates a list of directories
+        
+    Args:
+        path_to_directories (list): A list of directories to be created
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defults to False.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+           logger.info(f"Created directory: {path}")
+
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+
+    """
+    Get size in KB
+
+    Args:
+        path (Path): Path to the file whose size is to be found
+
+    Returns the size of a file in KB
+    """
+    size_in_kb = round(os.path.getsize(path) / 1024)
+    return f"{size_in_kb} KB"
