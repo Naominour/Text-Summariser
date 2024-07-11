ea0123459b571cdb02c4bf6c23a6ff7c80dde798
Naomi <naeimehnour@gmail.com>
2024-07-11 17:17:59 +0100

    stage 02 pipeline completed

---
+++ src/textsummarizer/pipeline/stage_02_data_validation.py
@@ -0,0 +1,16 @@
+from textsummarizer.config.configuration import Configurationmanager
+from textsummarizer.components.data_validation import DataValidation
+from textsummarizer.loggin import logger
+
+
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = Configurationmanager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(data_validation_config)
+        data_validation.download_file()
+        data_validation.extract_zip_file()
